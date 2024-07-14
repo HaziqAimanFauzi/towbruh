a8dd44a4958e7559c84ab54aee9d9a02a916e95f
HaziqAimanFauzi <136706089+HaziqAimanFauzi@users.noreply.github.com>
2024-07-14 13:23:56 +0800

    Update register.dart

--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:firebase_messaging/firebase_messaging.dart'; // Import Firebase Messaging
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 class RegisterPage extends StatefulWidget {
   final VoidCallback showLoginPage;
